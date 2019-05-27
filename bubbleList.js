87aa978d251d376518323333d609d5ce37422bdd
Noah Tarr <noahtarr1@gmail.com>
2019-05-26 20:25:19 -0700

    Added a bunch of new stuff
    Bubbles position based on true center not top left corner
    Animation that scales bubbles
    Images can be placed as bubble background, but scale is off currently.

--- bubbleList.js
+++ bubbleList.js
@@ -1,11 +1,14 @@
 var bubbleCount = $('.bubble').length;
 var angle = 360/bubbleCount;
-var radius = 150;
+var radius = 200;
 
 var bubbleIndex = 0;
-$('.bubbleList').children().each(function(){
+$('.bubbleList').children('.bubbleContainer').each(function(){
+    $(this).css('transform', 'rotate(' + angle*bubbleIndex + 'deg)' + 'translate(' + radius + 'px)');
+    $(this).children('.bubble').css('transform', 'rotate(' + -angle*bubbleIndex + 'deg)');
+    $(this).css('background-image', 'url(/"' +  + '/")');
+    var backgroundImage = $(this).find('.bubbleBackgroundImage').attr('src');
+    if (backgroundImage != null)
+        $(this).children('.bubble').css('background-image', 'url("' + backgroundImage + '")');
     bubbleIndex++;
-    $(this).css('transform', 'rotate(' + angle*bubbleIndex + 'deg)');
-    $(this).children('.bubble').css('transform', 'rotate(' + -angle*bubbleIndex + 'deg)'
-        + 'translate(' + radius + 'px)');
 });
